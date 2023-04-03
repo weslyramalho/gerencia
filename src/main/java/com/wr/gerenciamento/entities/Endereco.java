f60146e3f1d90dd8524a2b7b1ff2140916d440c7
weslyramalho <weslyramalho@yahoo.com.br>
2023-04-03 16:44:21 -0300

    iniciando

---
+++ src/main/java/com/wr/gerenciamento/entities/Endereco.java
@@ -0,0 +1,72 @@
+package com.wr.gerenciamento.entities;
+
+import java.util.UUID;
+
+public class Endereco {
+    private UUID id;
+    private String logradouro;
+    private String cep;
+    private Integer numero;
+    private String cidade;
+    private String estado;
+
+    public Endereco() {
+    }
+
+    public Endereco(UUID id, String logradouro, String cep, Integer numero, String cidade, String estado) {
+        this.id = id;
+        this.logradouro = logradouro;
+        this.cep = cep;
+        this.numero = numero;
+        this.cidade = cidade;
+        this.estado = estado;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
---
+++ src/main/java/com/wr/gerenciamento/entities/Pessoa.java
@@ -0,0 +1,64 @@
+package com.wr.gerenciamento.entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+public class Pessoa {
+    private UUID id;
+    private String nome;
+    private Date dataNascimento;
+    private Endereco enderecoPrincipal;
+    private List<Endereco> endereco = new ArrayList<>();
+
+    public Pessoa() {
+    }
+
+    public Pessoa(UUID id, String nome, Date dataNascimento, Endereco enderecoPrincipal) {
+        this.id = id;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.enderecoPrincipal = enderecoPrincipal;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Endereco getEnderecoPrincipal() {
+        return enderecoPrincipal;
+    }
+
+    public void setEnderecoPrincipal(Endereco enderecoPrincipal) {
+        this.enderecoPrincipal = enderecoPrincipal;
+    }
+
+    public List<Endereco> getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(List<Endereco> endereco) {
+        this.endereco = endereco;
+    }
+}
